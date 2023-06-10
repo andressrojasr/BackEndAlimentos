7affd0f938989e03cbf66a67e9c94cd7acfc745a
Andres <aramirez7310@uta.edu.ec>
2023-06-09 22:38:42 -0500

    Cambio en if de LoginView

--- Usuarios/views.py
+++ Usuarios/views.py
@@ -3,19 +3,20 @@ from django.utils.decorators import method_decorator
 from django.contrib.auth.hashers import check_password
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .models import TIP_ALIMENTOS
+from .models import USUARIOS
 
 class LoginView(APIView):
     def post(self, request):
         # Obtener los datos enviados desde Ionic
-        usuario = request.data.get('Nom_Tip')
-        contrasena = request.data.get('Des_Tip')
+        usuario = request.data.get('Usuario')
+        contraseña = request.data.get('Con_Usu')
+
         # Validar los datos con el modelo
         try:
-            usuario_obj = TIP_ALIMENTOS.objects.get(Nom_Tip=usuario)
-            if check_password(contrasena, usuario_obj.Des_Tip):
+            usuario_obj = USUARIOS.objects.get(Usuario=usuario)
+            if (contraseña ==usuario_obj.Con_Usu):
                 return Response({'mensaje': 'Los datos son válidos'})
             else:
                 return Response({'mensaje': 'Contraseña incorrecta'})
-        except TIP_ALIMENTOS.DoesNotExist:
+        except USUARIOS.DoesNotExist:
             return Response({'mensaje': 'Usuario no encontrado'})
