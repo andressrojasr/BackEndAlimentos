faa495b920ae5eee4cc8566acd678d23e8650bc3
Andres <aramirez7310@uta.edu.ec>
2023-06-12 22:26:05 -0500

    Agregue libreria JWT

--- backendAlimentos/settings.py
+++ backendAlimentos/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import rest_framework
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -144,4 +145,3 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
 }
-
