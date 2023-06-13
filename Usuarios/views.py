f73fe5f0c34f93b335c848f5ee01cf70c5cc1f46
Andres <aramirez7310@uta.edu.ec>
2023-06-12 22:02:17 -0500

    Cambios en metodo LoginView y creación de metodo ObtenerUsuarios

--- Usuarios/views.py
+++ Usuarios/views.py
@@ -3,7 +3,9 @@ from django.utils.decorators import method_decorator
 from django.contrib.auth.hashers import check_password
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .models import USUARIOS
+from .models import USUARIOS, REG_USUARIOS
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 class LoginView(APIView):
     def post(self, request):
@@ -15,8 +17,23 @@ class LoginView(APIView):
         try:
             usuario_obj = USUARIOS.objects.get(Usuario=usuario)
             if (contraseña ==usuario_obj.Con_Usu):
-                return Response({'mensaje': 'Los datos son válidos'})
+                token = RefreshToken.for_user(usuario_obj)
+                tiene_registros= REG_USUARIOS.objects.filter(Usuario=usuario_obj).exists()
+                return Response({'mensaje': 'Los datos son válidos', 'token': str(token.access_token), 'tieneRegistro': tiene_registros})
             else:
                 return Response({'mensaje': 'Contraseña incorrecta'})
         except USUARIOS.DoesNotExist:
             return Response({'mensaje': 'Usuario no encontrado'})
+        
+class ObtenerDatosUsuario(APIView):
+    authentication_classes = [JWTAuthentication]
+
+    def get(self, request):
+        usuario = request.user
+        usuario_data = {
+            'Usuario': usuario.Usuario,
+            'Nom_Usu': usuario.Nom_Usu,
+            'Ape_Usu': usuario.Ape_Usu,
+            'Fec_Nac_usu': usuario.Fec_Nac_usu
+        }
+        return Response({'usuario': usuario_data})
--- backendAlimentos/settings.py
+++ backendAlimentos/settings.py
@@ -139,5 +139,9 @@ if not DEBUG:
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
 
