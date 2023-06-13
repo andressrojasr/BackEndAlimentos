fb21f53a962594144000129262dbbabdc1a902e2
Andres <aramirez7310@uta.edu.ec>
2023-06-13 00:18:39 -0500

    Personalización de admin

--- Usuarios/admin.py
+++ Usuarios/admin.py
@@ -1,9 +1,44 @@
 from django.contrib import admin
 from .models import USUARIOS, TIP_ALIMENTOS, REGISTRO_ALIMENTOS, ALIMENTOS, DETALLE_ALIMENTOS, REG_USUARIOS
 
-admin.site.register(USUARIOS)
-admin.site.register(TIP_ALIMENTOS)
-admin.site.register(REGISTRO_ALIMENTOS)
-admin.site.register(ALIMENTOS)
-admin.site.register(DETALLE_ALIMENTOS)
-admin.site.register(REG_USUARIOS)
+@admin.register(USUARIOS)
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display=('Usuario','Nom_Usu','Ape_Usu','Fec_Nac_usu')
+    ordering=('Usuario',)
+    search_fields=('Usuario','Nom_Usu','Ape_Usu')
+    list_display_links=('Usuario',)
+    #list_editable('Usuario',)
+
+@admin.register(REG_USUARIOS)
+class RegUsuariosAdmin(admin.ModelAdmin):
+    list_display=('Usuario','Est_Usu','Pes_Usu','Ind_Mas_Cor','Fec_Reg')
+    ordering=('Usuario',)
+    search_fields=('Usuario','Fec_Reg')
+
+@admin.register(REGISTRO_ALIMENTOS)
+class RegistroAlimentosAdmin(admin.ModelAdmin):
+    list_display=('Usuario','Fec_Reg')
+    ordering=('Usuario',)
+    search_fields=('Usuario','Fec_Reg')
+
+
+@admin.register(TIP_ALIMENTOS)
+class TipAlimentosAdmin(admin.ModelAdmin):
+    list_display=('Nom_Tip','Des_Tip')
+    ordering=('Nom_Tip',)
+    search_fields=('Nom_Tip','Des_Tip')
+
+@admin.register(ALIMENTOS)
+class AlimentosAadmin(admin.ModelAdmin):
+    list_display=('Img_Ali','Nom_Ali','Cal_Ali','Cod_Tip')
+    ordering=('Nom_Ali',)
+    search_fields=('Nom_Ali','Cod_Tip','Cod_Tip')
+
+@admin.register(DETALLE_ALIMENTOS)
+class DetalleAlimentosAdmin(admin.ModelAdmin):
+    list_display=('Cod_Reg','Id_Ali','Cant_Calo')
+    ordering=('Cod_Reg',)
+    search_fields=('Cod_Reg','Id_Ali')
+
+
+
