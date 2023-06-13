f0e783447105e1c80dc59ef59f3cb7588c52e11b
Andres <aramirez7310@uta.edu.ec>
2023-06-12 23:00:00 -0500

    Agregue super usuario en build.sh

--- build.sh
+++ build.sh
@@ -6,3 +6,8 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+echo "
+from django.contrib.auth import get_user_model; 
+User = get_user_model(); 
+User.objects.create_superuser('andres', 'aramirez7310@uta.edu.ec', 'andres')
+" | python manage.py shell
