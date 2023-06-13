8a37772cbe77b1ba8f9656a4bdec6ffc1b62a2b1
Andres <aramirez7310@uta.edu.ec>
2023-06-12 23:25:16 -0500

    Quite makemigrations y puse las vistas de admin

--- Usuarios/admin.py
+++ Usuarios/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import USUARIOS, TIP_ALIMENTOS, REGISTRO_ALIMENTOS, ALIMENTOS, DETALLE_ALIMENTOS, REG_USUARIOS
 
-# Register your models here.
+admin.site.register(USUARIOS)
+admin.site.register(TIP_ALIMENTOS)
+admin.site.register(REGISTRO_ALIMENTOS)
+admin.site.register(ALIMENTOS)
+admin.site.register(DETALLE_ALIMENTOS)
+admin.site.register(REG_USUARIOS)
--- Usuarios/models.py
+++ Usuarios/models.py
@@ -7,6 +7,10 @@ class USUARIOS(models.Model):
     Ape_Usu = models.CharField(max_length=20)
     Con_Usu = models.CharField(max_length=20)
     Fec_Nac_usu = models.DateField()
+
+    def __srt__(self):
+        txt ="{0} (Nombre: {1} Apellido: {2})"
+        return txt.format(self.Usuario, self.Nom_Usu, self.Ape_Usu)
     
 class REG_USUARIOS(models.Model):
     Fec_Reg = models.DateField()
@@ -15,13 +19,25 @@ class REG_USUARIOS(models.Model):
     Ind_Mas_Cor= models.FloatField(null=True)
     Usuario = models.ForeignKey(USUARIOS, on_delete=models.CASCADE)
 
+    def __srt__(self):
+        txt ="{0} (Fecha: {1})"
+        return txt.format(self.Usuario, self.Fec_Reg)
+
 class REGISTRO_ALIMENTOS(models.Model):
     Fec_reg = models.DateField()
     Usuario = models.ForeignKey(USUARIOS, on_delete=models.CASCADE)
 
+    def __srt__(self):
+        txt ="{0} (Fecha: {1})"
+        return txt.format(self.Usuario, self.Fec_reg)
+
 class TIP_ALIMENTOS(models.Model):
     Nom_tip = models.CharField(max_length=20)
     Des_Tip = models.CharField(max_length=50, null=True)
+
+    def __srt__(self):
+        txt ="{0}"
+        return txt.format(self.Nom_tip)
 
 class ALIMENTOS(models.Model):
     Nom_Ali = models.CharField(max_length=20)
@@ -29,12 +45,24 @@ class ALIMENTOS(models.Model):
     Img_Ali = models.ImageField()
     Cod_Tip = models.ForeignKey(TIP_ALIMENTOS, on_delete=models.CASCADE)
 
+    def __srt__(self):
+        txt ="{0} (Calorias: {1})"
+        return txt.format(self.Nom_Ali, self.Cal_Ali)
+
 class DETALLE_ALIMENTOS(models.Model):
     Cod_Reg = models.ForeignKey(REGISTRO_ALIMENTOS, on_delete=models.CASCADE)
     Id_Ali = models.ForeignKey(ALIMENTOS, on_delete=models.CASCADE)
     Cant_calo = models.FloatField(null=True)
 
+    def __srt__(self):
+        txt ="{0} (Id Alimento: {1} Cantidad calorias: {2})"
+        return txt.format(self.Cod_Reg, self.Id_Ali, self.Cant_calo, self.Cant_calo)
+
 class ADMINISTRADORES(models.Model):
     Usu_Adm = models.CharField(max_length=20, primary_key=True)
     Con_Adm = models.CharField(max_length=25)
     Cor_Adm = models.EmailField()
+    
+    def __srt__(self):
+        txt ="{0} (Usuario: {1} Correo: {2})"
+        return txt.format(self.Usu_Adm, self.Cor_Adm)
