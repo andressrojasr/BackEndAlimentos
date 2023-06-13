f92250e518a7e82f56ea0af50c1b025829144e44
Andres <aramirez7310@uta.edu.ec>
2023-06-12 23:10:52 -0500

    Quite el superusuario

--- build.sh
+++ build.sh
@@ -7,8 +7,3 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py makemigrations
-echo "
-from django.contrib.auth import get_user_model; 
-User = get_user_model(); 
-User.objects.create_superuser('andres', 'aramirez7310@uta.edu.ec', 'andres')
-" | python manage.py shell
