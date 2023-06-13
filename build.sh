c1a0bef021a259c83c077e8cada92b8d93fe53c9
Andres <aramirez7310@uta.edu.ec>
2023-06-12 22:34:49 -0500

    Cambios en build.sh

--- build.sh
+++ build.sh
@@ -6,3 +6,5 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py createsuperuser
+python manage.py djangorestframework_simplejwt create_keys
