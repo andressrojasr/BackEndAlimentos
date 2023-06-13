2f75b16965a86f61d64708234e96a0023df721be
Andres <aramirez7310@uta.edu.ec>
2023-06-13 16:04:17 -0500

    Cambio en urls Usuarios

--- Usuarios/urls.py
+++ Usuarios/urls.py
@@ -15,5 +15,5 @@ router.register('Administradores', AdministradoresViewSet,'Administradores')
 
 urlpatterns = [
     path('auth/loginView/', LoginView.as_view(), name='loginView'),
-    path('api/', include(router.urls)),
+    path('', include(router.urls)),
 ]
