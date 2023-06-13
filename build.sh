6dce8b2f7e354beb09bb407a68fa80bdac358641
Andres <aramirez7310@uta.edu.ec>
2023-06-12 23:08:10 -0500

    Agregue makemigrations en build.sh

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py makemigrations
 echo "
 from django.contrib.auth import get_user_model; 
 User = get_user_model(); 
