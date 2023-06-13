b2a5535d8e8eaade03c054468d340d6f4df30ff7
Andres <aramirez7310@uta.edu.ec>
2023-06-13 15:59:23 -0500

    Cambio de urls

--- backendAlimentos/urls.py
+++ backendAlimentos/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('Usuarios.urls'))
+    path('', admin.site.urls),
+    path('/registros',include('Usuarios.urls'))
 ]
