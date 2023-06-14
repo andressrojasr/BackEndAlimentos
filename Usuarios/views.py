8e16d350f5fe6f7e6b58402cb372aec43e6ff260
Andres <aramirez7310@uta.edu.ec>
2023-06-14 08:19:57 -0500

    Listo para produccion

--- Usuarios/views.py
+++ Usuarios/views.py
@@ -9,37 +9,37 @@ from rest_framework_simplejwt.tokens import RefreshToken
 
 
 
-#class LoginView(APIView):
- #   def post(self, request):
+class LoginView(APIView):
+    def post(self, request):
         # Obtener los datos enviados desde Ionic
-  #      usuario = request.data.get('Usuario')
-   #     contraseña = request.data.get('Con_Usu')
+        usuario = request.data.get('Usuario')
+        contraseña = request.data.get('Con_Usu')
 
         # Validar los datos con el modelo
-    #    try:
-     #       usuario_obj = USUARIOS.objects.get(Usuario=usuario)
-      #      if (contraseña ==usuario_obj.Con_Usu):
-      #          return Response({'mensaje': 'Los datos son válidos'})
-       #     else:
-        #        return Response({'mensaje': 'Contraseña incorrecta'})
-       # except USUARIOS.DoesNotExist:
-        #    return Response({'mensaje': 'Usuario no encontrado'})
-
-class LoginView(TokenObtainPairView):
-    def post(self, request, *args, **kwargs):
-        username = request.data.get('Usuario')
-        password = request.data.get('Con_Usu')
-
         try:
-            user = USUARIOS.objects.get(Usuario=username)
-            if (user.Con_Usu == password):
-                refresc= RefreshToken.for_user(user)
-                return Response({
-                    'refresh': str(refresc),
-                    'access': str(refresc.access_token),
-                    'mensaje': 'Los datos son válidos',
-                })
+            usuario_obj = USUARIOS.objects.get(Usuario=usuario)
+            if (contraseña ==usuario_obj.Con_Usu):
+                return Response({'mensaje': 'Los datos son válidos'})
             else:
                 return Response({'mensaje': 'Contraseña incorrecta'})
         except USUARIOS.DoesNotExist:
             return Response({'mensaje': 'Usuario no encontrado'})
+
+#class LoginView(TokenObtainPairView):
+ #   def post(self, request, *args, **kwargs):
+  #      username = request.data.get('Usuario')
+   #     password = request.data.get('Con_Usu')
+
+    #    try:
+     #       user = USUARIOS.objects.get(Usuario=username)
+      #      if (user.Con_Usu == password):
+       #         refresc= RefreshToken.for_user(user)
+        #        return Response({
+         #           'refresh': str(refresc),
+          #          'access': str(refresc.access_token),
+           #         'mensaje': 'Los datos son válidos',
+            #    })
+           # else:
+            #    return Response({'mensaje': 'Contraseña incorrecta'})
+       # except USUARIOS.DoesNotExist:
+        #    return Response({'mensaje': 'Usuario no encontrado'})
