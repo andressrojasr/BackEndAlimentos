3680e66e05f7a4402bad4ff8ed85501768d56f03
Andres <aramirez7310@uta.edu.ec>
2023-06-14 07:24:09 -0500

    .Usuario

--- Usuarios/views.py
+++ Usuarios/views.py
@@ -33,7 +33,7 @@ class LoginView(TokenObtainPairView):
         try:
             user = USUARIOS.objects.get(Usuario=username)
             if (user.Con_Usu == password):
-                refresc= RefreshToken.for_user(user)
+                refresc= RefreshToken.for_user(user.Usuario)
                 return Response({
                     'refresh': str(refresc),
                     'access': str(refresc.access_token),
