8482c3a0334b72871987eebcb394364437cea596
Andres <aramirez7310@uta.edu.ec>
2023-06-14 07:30:32 -0500

    Cambio orden apps

--- backendAlimentos/settings.py
+++ backendAlimentos/settings.py
@@ -42,12 +42,12 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'Usuarios',
     'rest_framework',
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'Usuarios',
 ]
 
 REST_FRAMEWORK = {
