4d1bb56cf546eb40d6ffda57115678fd1fa97d1d
Andres <aramirez7310@uta.edu.ec>
2023-06-30 22:38:12 -0500

    Agregada vista listarRegistrosAlimentos

--- Usuarios/views.py
+++ Usuarios/views.py
@@ -178,9 +178,9 @@ class filtrarRegistrosAlimentos(APIView):
             usuario = request.data.get('Usuario')
             fechaIni = request.data.get('Fec_Ini')
             fechaFin = request.data.get('Fec_Fin')
-            reg_usuarios = REGISTRO_ALIMENTOS.objects.filter(Usuario=usuario, Fec_Reg__gte=fechaIni, Fec_Reg__lte=fechaFin).order_by('-Fec_reg')
+            reg_usuarios = REGISTRO_ALIMENTOS.objects.filter(Usuario=usuario, Fec_reg__gte=fechaIni, Fec_reg__lte=fechaFin).order_by('-Fec_reg')
             serializer = RegistroAlimentosSerializer(reg_usuarios, many=True)
-            detalle_reg = DETALLE_ALIMENTOS.objects.filter(Cod_Reg__Usuario=usuario,Cod_Reg__Fec_Reg__gte=fechaIni, Cod_Reg__Fec_Reg_lte=fechaFin)
+            detalle_reg = DETALLE_ALIMENTOS.objects.filter(Cod_Reg__Usuario=usuario,Cod_Reg__Fec_reg__gte=fechaIni, Cod_Reg__Fec_reg_lte=fechaFin)
             detalleSerializer=DetalleAlimentosSerializer(detalle_reg,many=True)
             return Response({'registros':serializer.data,'detalles':detalleSerializer.data})
         except Exception as e:
