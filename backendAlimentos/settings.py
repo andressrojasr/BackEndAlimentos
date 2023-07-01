03f027e91995b43878d86765dcdf26cd43f3be6c
Andres <aramirez7310@uta.edu.ec>
2023-07-01 11:51:21 -0500

    Agregado imagenes

--- backendAlimentos/settings.py
+++ backendAlimentos/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'Usuarios',
 ]
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
